7262943b98d85c87579805639954ec1f3ca7c69f
rdfenix <rhoodsk8erboy@gmail.com>
2019-01-25 13:36:31 -0200

    estudo sobre parse de dados no nodejs

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>')
+})
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
 
 app.use('/', (req, res, next) => {
     res.send('<h1>Hello express</h1>')
 })
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<h1>Add product</h1>')
-})
-
 app.listen(3000)
-
-
-/*const http = require('http');
-const express = require('express');
-const app = express();
-
-//const routes = require('./routes');
-
-//console.log(routes.someText);
-
-const server = http.createServer(app);
-
-server.listen(3000);*/
