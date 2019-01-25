a32847d406a6563b9f05c9582ab3987583409596
rdfenix <rhoodsk8erboy@gmail.com>
2019-01-25 00:17:06 -0200

    curso de node

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello express</h1>')
+})
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>Add product</h1>')
+})
+
+app.listen(3000)
+
+
+/*const http = require('http');
 const express = require('express');
 const app = express();
 
@@ -8,4 +22,4 @@ const app = express();
 
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(3000);*/
