544fd4d32f6e543b4fa3228659a85a41555cfa6a
rdfenix <rhoodsk8erboy@gmail.com>
2019-01-28 14:35:36 -0200

    começando estudo sobre rotas no express

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+const adminRouter = require('./routes/admin')
+
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(adminRouter)
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>')
-})
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-})
 
 app.use('/', (req, res, next) => {
     res.send('<h1>Hello express</h1>')
