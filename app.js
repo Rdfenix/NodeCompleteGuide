d533b266e2b2b45846e5845d176e0fcb4ff6a255
rdfenix <rhoodsk8erboy@gmail.com>
2019-01-28 23:41:32 -0200

    estudando rotas em express

--- app.js
+++ app.js
@@ -3,14 +3,10 @@ const bodyParser = require('body-parser')
 const app = express()
 
 const adminRouter = require('./routes/admin')
+const shopRouter = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(adminRouter)
-
-
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello express</h1>')
-})
+app.use(shopRouter)
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-
 router.get('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>')
 })
