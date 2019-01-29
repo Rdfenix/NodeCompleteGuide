e057ad9e1a76e66c38eb3ee083c561000abbbed6
rdfenix <rhoodsk8erboy@gmail.com>
2019-01-28 23:53:12 -0200

    criando filtro de path

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ const app = express()
 const adminRouter = require('./routes/admin')
 const shopRouter = require('./routes/shop')
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(adminRouter)
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use('/admin', adminRouter)
 app.use(shopRouter)
+
+app.use((req, res) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000)
