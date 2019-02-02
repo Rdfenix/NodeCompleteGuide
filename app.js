633cd805340328d75fb03ba6720a434d736b5383
rdfenix <rhoodsk8erboy@gmail.com>
2019-02-02 16:30:30 -0200

    criando diretorio comum

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const path = require('path')
 
 const adminRouter = require('./routes/admin')
 const shopRouter = require('./routes/shop')
@@ -13,7 +14,8 @@ app.use('/admin', adminRouter)
 app.use(shopRouter)
 
 app.use((req, res) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    //res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000)
