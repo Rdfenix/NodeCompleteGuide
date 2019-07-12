b425244217e3a069f87046270e732164d892f611
rdfenix <rhoodsk8erboy@gmail.com>
2019-07-12 14:30:48 -0300

    adding path undefined in 404 route

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ app.use((req, res) => {
     //res.status(404).send('<h1>Page not found</h1>')
     //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
     res.status(404).render('404', {
-        pageTitle: 'Page Not Found'
+        pageTitle: 'Page Not Found',
+        path: undefined
     })
 })
 
