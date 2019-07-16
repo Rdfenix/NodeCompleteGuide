8e9b5444097571c643a95c472f0d818eb9a4ee0e
rdfenix <rhoodsk8erboy@gmail.com>
2019-07-16 13:57:54 -0300

    created FS function to save and get datas

--- ProjectMVC/routes/admin.js
+++ ProjectMVC/routes/admin.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const path = require('path')
-const productsController = require('../controllers/products')
-const router = express.Router()
+const path = require('path');
 
+const express = require('express');
 
-router.get('/add-product', productsController.getAddProduct )
+const productsController = require('../controllers/products');
 
-router.post('/add-product', )
+const router = express.Router();
 
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
 
-module.exports = router
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
+
+module.exports = router;
--- ProjectMVC/routes/shop.js
+++ ProjectMVC/routes/shop.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const router = express.Router()
+const path = require('path');
 
-const productsController = require('../controllers/products')
+const express = require('express');
 
-router.get('/', productsController.getProducts)
+const productsController = require('../controllers/products');
 
-module.exports = router
+const router = express.Router();
+
+router.get('/', productsController.getProducts);
+
+module.exports = router;
